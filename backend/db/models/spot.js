242b957418a8ecd42e398793dfab6cfd11c2cb08
DmitriMenas <dmitri.menas@gmail.com>
2025-01-02 17:48:01 -0800

    spot fix

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -1,82 +1,72 @@
-"use strict";
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  const Spot = sequelize.define(
-    "Spot",
-    {
-      id: {
-        primaryKey: true,
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-      },
-      ownerId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-          model: "User",
-          key: "id"
-        }
-      },
-      address: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      city: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      state: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      country: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      lat: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-      },
-      lng: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      description: {
-        type: DataTypes.TEXT,
-        allowNull: false,
-      },
-      price: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
+  class Spot extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Spot.init({
+    id: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    ownerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "User",
+        key: "id"
       }
     },
-    {}
-  );
-
-  Spot.associate = function (models) {
-    // Define associations here
-
-    Spot.belongsTo(models.User, { foreignKey: "ownerId", as: "Owner" });
-    Spot.hasMany(models.Review, {
-      foreignKey: "spotId",
-      onDelete: "CASCADE",
-      hooks: true,
-    });
-    Spot.hasMany(models.Booking, {
-      foreignKey: "spotId",
-      onDelete: "CASCADE",
-      hooks: true,
-    });
-    Spot.hasMany(models.SpotImage, {
-      foreignKey: "spotId",
-      onDelete: "CASCADE",
-      hooks: true,
-    });
-  };
-
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lat: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    lng: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    }
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
   return Spot;
 };
