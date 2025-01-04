099725e1692cc1d93dbb789448344f25e2358046
DmitriMenas <dmitri.menas@gmail.com>
2025-01-03 20:23:43 -0800

    commit somthing i tell u what

--- backend/db/migrations/20241219021233-create-booking.js
+++ backend/db/migrations/20241219021233-create-booking.js
@@ -1,4 +1,11 @@
 'use strict';
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -34,7 +41,7 @@ module.exports = {
         type: Sequelize.DATE,
         allowNull: false,
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Bookings');
