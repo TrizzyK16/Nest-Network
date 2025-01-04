14f44c55aa686c30fa214043047ff926c55788c0
DmitriMenas <dmitri.menas@gmail.com>
2025-01-03 20:29:34 -0800

    progress?

--- backend/db/seeders/20241219024557-demo-spot-images.js
+++ backend/db/seeders/20241219024557-demo-spot-images.js
@@ -1,44 +1,89 @@
 'use strict';
+const { SpotImage } = require('../models');
+//LINES 6-9 (NICE) SHOULD BE INCLUDED BUT NOT NEEDED SPECIFICALLY FOR SEEDERS
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('SpotImages', [
+     return SpotImage.bulkCreate([
       {
-        spotId: 1,
-        url: 'https://example.com/image1.jpg',
-        preview: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 1,
-        url: 'https://example.com/image2.jpg',
-        preview: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 2,
-        url: 'https://example.com/image3.jpg',
-        preview: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 2,
-        url: 'https://example.com/image4.jpg',
-        preview: false,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        spotId: 3,
-        url: 'https://example.com/image5.jpg',
-        preview: true,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }
-    ]);
+            spotId: 1,
+            url: 'https://example.com/image1.jpg',
+            preview: true,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          {
+            spotId: 1,
+            url: 'https://example.com/image2.jpg',
+            preview: false,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          {
+            spotId: 2,
+            url: 'https://example.com/image3.jpg',
+            preview: true,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          {
+            spotId: 2,
+            url: 'https://example.com/image4.jpg',
+            preview: false,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          {
+            spotId: 3,
+            url: 'https://example.com/image5.jpg',
+            preview: true,
+            createdAt: new Date(),
+            updatedAt: new Date()
+          }
+     ]);
+
+
+    // return queryInterface.bulkCreate('SpotImages', [
+    //   {
+    //     spotId: 1,
+    //     url: 'https://example.com/image1.jpg',
+    //     preview: true,
+    //     createdAt: new Date(),
+    //     updatedAt: new Date()
+    //   },
+    //   {
+    //     spotId: 1,
+    //     url: 'https://example.com/image2.jpg',
+    //     preview: false,
+    //     createdAt: new Date(),
+    //     updatedAt: new Date()
+    //   },
+    //   {
+    //     spotId: 2,
+    //     url: 'https://example.com/image3.jpg',
+    //     preview: true,
+    //     createdAt: new Date(),
+    //     updatedAt: new Date()
+    //   },
+    //   {
+    //     spotId: 2,
+    //     url: 'https://example.com/image4.jpg',
+    //     preview: false,
+    //     createdAt: new Date(),
+    //     updatedAt: new Date()
+    //   },
+    //   {
+    //     spotId: 3,
+    //     url: 'https://example.com/image5.jpg',
+    //     preview: true,
+    //     createdAt: new Date(),
+    //     updatedAt: new Date()
+    //   }
+    // ]);
   },
 
   down: async (queryInterface, Sequelize) => {
