616a2c5e8defae25f7dec1bedb43a5fd98f60cf5
TrizzyK16 <trizzy516@gmail.com>
2025-02-01 14:57:38 -0500

    Phase 0 auth me

---
+++ frontend/src/store/store.js
@@ -0,0 +1,26 @@
+// frontend/src/store.store.js
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { thunk } from 'redux-thunk';
+
+//Root reducer
+const rootReducer = combineReducers({
+
+});
+
+// Enhancer
+let enhancer;
+if (import.meta.env.MODE === 'production') {
+  enhancer = applyMiddleware(thunk);
+} else {
+  const logger = (await import("redux-logger")).default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+}
+
+// configureStore function
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+  };
+
+export default configureStore;
